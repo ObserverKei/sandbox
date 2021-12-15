2e4f55c30a568e9170dda0662d46c2d57317b2fd
observerkei <observerkei@foxmail.com>
2021-12-16 05:39:31 +0800

    Update cat.sh

--- cat.sh
+++ cat.sh
@@ -20,6 +20,11 @@ fn_cat_update()
 	exit 0
 }
 
+fn_cat_sleep()
+{
+	sleep `h_cat_self cat_sleep_time`
+}
+
 fn_cat_see()
 {
 	ls > `h_cat_self cat_see_log`
@@ -31,21 +36,25 @@ h_cat_self()
 	local __cat_space_path=${g_cat_cutdir}space
 	local __cat_see_log=$__cat_core_path/sandbox_see.log
 	local __cat_update_shell_file=$__cat_core_path/update.sh
+	local __cat_sleep_time=1
 	
 	local __fn_cat_check_update=fn_cat_check_update
 	local __fn_cat_update=fn_cat_update
 	local __fn_cat_core_start=fn_cat_core_start
+	local __fn_cat_sleep=fn_cat_sleep
 	local __fn_cat_see=fn_cat_see
 	
 	case $1 in
 		cat_core_path) echo $__cat_core_path;;
 		cat_space_path) echo $__cat_space_path;;
-		cat_see_log) echo $__cat_see_log;;
 		cat_update_shell_file) echo $__cat_update_shell_file;;
-		fn_cat_check_update) echo $__fn_cat_check_update;;
+		cat_see_log) echo $__cat_see_log;;
+		cat_sleep_time) echo $__cat_sleep_time;;
 		
-		fn_cat_update) echo $__fn_cat_update;;
 		fn_cat_core_start) echo $__fn_cat_core_start;;
+		fn_cat_check_update) echo $__fn_cat_check_update;;
+		fn_cat_update) echo $__fn_cat_update;;
+		fn_cat_sleep) echo $__fn_cat_sleep;;
 		fn_cat_see) echo $__fn_cat_see;;
 		*) echo ;;
 	esac
@@ -53,6 +62,7 @@ h_cat_self()
 
 fn_cat_core_start()
 {
+	`h_cat_self fn_cat_sleep`
 	`h_cat_self fn_cat_see`
 	[ 0 -ne $(`h_cat_self fn_cat_check_update`) ] && `h_cat_self fn_cat_update`
 }
@@ -64,7 +74,5 @@ mkdir -p `h_cat_self cat_core_path` > /dev/null 2>&1
 
 pushd `h_cat_self cat_space_path` > /dev/null 2>&1
 
-while :; do 
-	sleep 1;
-	`h_cat_self fn_cat_core_start`
-done
+
+while :; do `h_cat_self fn_cat_core_start`; done
